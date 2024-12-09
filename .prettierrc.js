95a9d939cc9ca59960b9ed090453b6c0b6c290cf
andoikm <andoikm@gmail.com>
2024-12-10 00:56:46 +0400

    handle input icons

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,16 @@
 module.exports = {
-  printWidth: 120,
-  tabWidth: 2,
-  trailingComma: 'es5',
-  singleQuote: true,
-  arrowParens: 'avoid',
-  semi: true,
+  printWidth: 120,              // Limit line length to 120 characters
+  tabWidth: 2,                  // Indent using 2 spaces
+  trailingComma: 'es5',         // Include trailing commas in ES5 (objects, arrays, etc.)
+  singleQuote: true,            // Use single quotes wherever possible
+  arrowParens: 'avoid',         // Avoid parentheses for single-parameter arrow functions
+  semi: true,                   // Enforce semicolons at the end of statements
+  jsxSingleQuote: false,        // Use double quotes in JSX attributes
+  bracketSpacing: true,         // Add spaces between brackets in object literals
+  endOfLine: 'auto',            // Maintain existing line endings (useful for cross-platform projects)
+  proseWrap: 'preserve',        // Do not force wrapping of markdown text
+  useTabs: false,               // Use spaces instead of tabs for indentation
+  quoteProps: 'consistent',     // Only quote object properties if necessary, or consistently if one requires it
+  jsxBracketSameLine: false,    // Put the closing bracket of JSX elements on a new line
+  embeddedLanguageFormatting: 'auto', // Automatically format embedded code (like HTML or CSS in JS)
 };
