68191cfa2e8a40d3c022753eb3f18e918c6dc9d9
andoikm <andoikm@gmail.com>
2024-12-09 13:14:01 +0400

    animateed

---
+++ app/screens/LiveMatch.js
@@ -0,0 +1,13 @@
+import {View} from "react-native";
+import Layout from "../components/Layout";
+
+const LiveMatch = () => {
+
+	return (
+		<Layout>
+			<View>Live mtach</View>
+		</Layout>
+	);
+};
+
+export default LiveMatch;
